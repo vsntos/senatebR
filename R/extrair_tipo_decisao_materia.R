8660b6a4a4a4880ef3a0b8c6e1e044b47d5ffc9f
Vinicius Santos <santos.vinicius18@gmail.com>
2024-04-22 09:26:50 -0300

    atualizacao

--- R/extrair_tipo_decisao_materia.R
+++ R/extrair_tipo_decisao_materia.R
@@ -1,12 +1,12 @@
 #' Extrair os tipos de decisão de uma URL específica
 #'
-#' Esta função realiza o scraping de uma página XML contendo informações sobre os tipos de decisão e extrai os dados relevantes.
+#' Esta função realiza o scraping sobre os tipos de decisão e extrai os dados relevantes.
 #'
 #' @return Um dataframe contendo os tipos de decisão.
 #' @importFrom xml2 read_xml xml_find_all xml_text xml_find_first
 #' @export
 #' @examples
-#' # Extrair os tipos de decisão a partir da URL padrão
+#' # Extrair os tipos de decisão
 #' df_tipos_decisao <- extrair_tipos_decisao()
 extrair_tipos_decisao <- function(url = "https://legis.senado.leg.br/dadosabertos/dados/ListaTiposDecisao.xml") {
   # Faça o download e analise o XML
